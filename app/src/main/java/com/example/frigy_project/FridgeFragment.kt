7ea31e8393b9b5b20cf064be0dba87fcc645cd51
Seraphic <danilsocol-2002@mail.ru>
2023-04-15 00:06:05 +0500

    Start

---
+++ app/src/main/java/com/example/frigy_project/FridgeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.frigy_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.frigy_project.databinding.FragmentFridgeBinding
+
+
+class FridgeFragment: Fragment() {
+
+    private var binding: FragmentFridgeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_fridge, container, false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+
+}
