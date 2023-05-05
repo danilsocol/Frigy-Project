da2c59a14164f636cd32766783f9514369a678ac
Seraphic <danilsocol-2002@mail.ru>
2023-05-05 14:51:45 +0500

    delete interface

--- app/src/main/java/com/example/frigy_project/adapters/BaseAdapter.kt
+++ app/src/main/java/com/example/frigy_project/adapters/BaseAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.frigy_project.adapters
 
+import android.widget.Filterable
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.frigy_project.models.Product
 
-abstract class BaseAdapter<T : Any> : ListAdapter<T, RecyclerView.ViewHolder>(DiffCallback<T>()), IFilterable<T>
+abstract class BaseAdapter<T : Any> : ListAdapter<T, RecyclerView.ViewHolder>(DiffCallback<T>()),
+    Filterable
 {
     var originalList : List<T>? = null
+    abstract fun setData(newList: List<T>)
 }
--- app/src/main/java/com/example/frigy_project/adapters/IFilterable.kt
+++
@@ -1,9 +0,0 @@
-package com.example.frigy_project.adapters
-
-import android.widget.Filterable
-import com.example.frigy_project.models.Product
-
-interface IFilterable<T> : Filterable {
-
-    fun setData(newList: List<T>)
-}
