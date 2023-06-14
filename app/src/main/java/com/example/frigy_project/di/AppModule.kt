70f94a8a500c031550f7d3bc47d8fc8491b5e740
Seraphic <danilsocol-2002@mail.ru>
2023-06-14 17:02:54 +0500

    DI viewModel factory

--- app/src/main/java/com/example/frigy_project/di/AppModule.kt
+++ app/src/main/java/com/example/frigy_project/di/AppModule.kt
@@ -1,7 +1,15 @@
 package com.example.frigy_project.di
 
+import androidx.lifecycle.ViewModel
+import com.example.frigy_project.presentation.utils.ViewModelKey
+import com.example.frigy_project.presentation.viewModels.RecipeFragmentViewModel
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
-class AppModule {
+abstract class AppModule {
+
+    @IntoMap
+    @ViewModelKey(RecipeFragmentViewModel::class)
+    abstract fun recipeFragmentViewModel(viewModel: com.example.frigy_project.presentation.viewModels.RecipeFragmentViewModel): ViewModel
 }
